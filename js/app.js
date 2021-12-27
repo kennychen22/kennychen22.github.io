03554597fcf96120564c9c9f39c816269a1ebbb6
kennychen22 <kchen22@amherst.edu>
2021-12-27 11:00:29 -0500

    Make the splash screen appear only once.

--- js/app.js
+++ js/app.js
@@ -2,26 +2,34 @@ let intro = document.querySelector('.intro');
 let logo = document.querySelector('.logo-header');
 let logoSpan = document.querySelectorAll('.logo');
 
-window.addEventListener('DOMContentLoaded', ()=>{
-  setTimeout(()=>{
-    logoSpan.forEach((span, idx)=>{
-      setTimeout(() =>{
+
+// $('#myid').one('click',
+// $('window').one()
+
+window.addEventListener('DOMContentLoaded', function splash() {
+
+  // Remove the listener from the element the first time the listener is run:
+  window.removeEventListener('DOMContentLoaded', splash);
+  
+  setTimeout(function first_timeout() {
+    logoSpan.forEach(function first_span(span, idx) {
+      setTimeout(function first_active() {
         span.classList.add('active');
       }, (idx+1) * 400)
     });
 
-    setTimeout(() => {
-      logoSpan.forEach((span, idx) => {
-        setTimeout(() => {
-          span.classList.remove('active');
-          span.classList.add('fade');
-        }, (idx+1)*50)
-      })
-    }, 2000);
 
-    setTimeout(()=>{
+    // setTimeout(() => {
+    //   logoSpan.forEach((span, idx) => {
+    //     setTimeout(() => {
+    //       span.classList.remove('active');
+    //       span.classList.add('fade');
+    //     }, (idx+1)*50)
+    //   })
+    // }, 2000);
+
+    setTimeout(function last_timeout(){
       intro.style.top = '-100vh';
-    }, 2300);
-
+    }, 2300)
   })
 })
