74562464164c7b24eda1c994ebd1f35c4070de1d
kennychen22 <kchen22@amherst.edu>
2021-12-28 22:46:45 -0500

    Add active navbar highlight

--- js/app.js
+++ js/app.js
@@ -49,3 +49,20 @@ window.addEventListener('scroll', function () {
     }
   }
 })
+
+// Add Active Class to Current Element
+
+// Get the container element
+var btnContainer = document.getElementById("home").getElementsByClassName("topnav");
+
+// Get all buttons with class="btn" inside the container
+var btns = document.getElementsByClassName("nav");
+// Console.
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
